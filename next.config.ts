df369ff231f5d0bdbb745e1e5d4f9ef9beb9df87
akbar-dzikri <akbardzkr05@gmail.com>
2025-06-21 05:22:27 +0700

    refactor(next.config): remove unused distDir and output options
    Clean up next.config.ts by removing deprecated or unused configuration options to simplify the configuration file

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "./dist",
   images: {
     unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: "standalone",
 };
 
 export default nextConfig;
